a66c093593f679fd4c9d62b52cea5bf788f8a6ea
SundayEfeTerry <info.techbro.set@gmail.com>
2023-05-28 09:59:55 +0100

    madlibs

--- _projects_/Mad_Libs_Story_Generator.py
+++ _projects_/Mad_Libs_Story_Generator.py
@@ -3,32 +3,21 @@ print('Enter some Noun and an Adjective ---> I\'ll make a cool story for you..')
 
 # Questions for the user to answer
 noun = input('Choose a noun: ')
-
 p_noun = input('Choose a plural noun: ')
-
 noun2 = input('Choose a noun: ')
-
 place = input('Name a place: ')
-
 adjective = input('Choose an adjective (Describing word): ')
-
 noun3 = input('Choose a noun: ')
 
 # Print a story from the user input
-print('----------------Story--------------------------')
+print('----------------Story-------------------------')
 
 print('Be kind to your', noun, ' or ', p_noun)
-
 print('For a duck may be somebody\'s', noun2, ',')
-
 print('Be kind to your', p_noun, 'in', place)
-
 print('Where everyone is always', adjective, '. \n')
-
-print('You may think that is this the', noun3, ',')
-
+print('You may think that is the new', noun3, ',')
 print('Well it is....:)')
-
 print('Be Kind always......')
 
-print('----------------Ok Bye-------------------------')
+print('---------------Ok Bye-------------------------')
---
+++ _projects_/madlibs_info.py
@@ -0,0 +1,10 @@
+name = input('name: ')
+age = input('age: ')
+add = input('address: ')
+location = input('location: ')
+e_mail = input('email: ')
+any_thing_else = input('others: ')
+
+madlib = f'hello: \n my name is {name} \n i am {age} years old \n i live at: {add} {location} \n email: {e_mail} \n Others: {any_thing_else}'
+
+print(madlib)
